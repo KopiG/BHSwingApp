b9625570db89cb8c483524595749470c1cfc08af
Gergo Kopacsi <gergo.kopacsi@loxon.eu>
2019-03-27 10:10:49 +0100

    added missing file

---
+++ src/com/braininghub/swing/frame/MainFrame.java
@@ -0,0 +1,32 @@
+package com.braininghub.swing.frame;
+
+import com.braininghub.swing.entity.Donor;
+import com.braininghub.swing.model.DonorTableModel;
+import com.braininghub.swing.service.DonorService;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+import java.awt.HeadlessException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created on 2019. márc. 27.
+ *
+ * @author <a href="gergo.kopacsi@loxon.eu">Gergő Kopácsi</a>
+ */
+public class MainFrame extends JFrame {
+    public MainFrame() throws HeadlessException {
+        DonorTableModel donorTableModel = new DonorTableModel(DonorService.getDonors());
+        JTable donorTable = new JTable(donorTableModel);
+        JScrollPane jScrollPane = new JScrollPane(donorTable);
+        JFrame frame = new JFrame("App");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.getContentPane().add(jScrollPane);
+        frame.setSize(600, 400);
+    }
+}
